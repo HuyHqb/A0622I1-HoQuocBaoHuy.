2416a64e59b1081e9948c8da679b538edc169d9a
professinal12 <50769481+professinal12@users.noreply.github.com>
2022-08-22 18:07:58 +0700

    bai tap

--- Module 1/ss16_OOP/bai_tap/game_don_gian/script.js
+++ Module 1/ss16_OOP/bai_tap/game_don_gian/script.js
@@ -1,35 +1,49 @@
 /**
  * Created by nhatnk on 4/26/17.
  */
-
- function Hero(image, top, left, size){
+class Hero {
+  constructor(image, top, left, size, speed) {
     this.image = image;
     this.top = top;
     this.left = left;
     this.size = size;
-  
-    this.getHeroElement = function(){
-      return '<img width="'+ this.size + '"' +
-        ' height="'+ this.size + '"' +
-        ' src="' + this.image +'"' +
-        ' style="top: '+this.top+'px; left:'+this.left+'px;position:absolute;" />';
-    }
-  
-    this.moveRight = function(){
-      this.left += 20;
-      console.log('ok: ' + this.left);
-    }
-  
+    this.speed = speed;
   }
-  
-  var hero = new Hero('pikachu.png', 20, 30, 200);
-  
-  function start(){
-    if(hero.left < window.innerWidth - hero.size){
-      hero.moveRight();
-    }
-    document.getElementById('game').innerHTML = hero.getHeroElement();
-    setTimeout(start, 500)
+  getHeroElement() {
+    return '<img width="' + this.size + '"' +
+      ' height="' + this.size + '"' +
+      ' src="' + this.image + '"' +
+      ' style="top: ' + this.top + 'px; left:' + this.left + 'px;position:absolute;" />';
   }
-  
-  start();
+  moveRight() {
+    this.left += this.speed;
+    console.log('ok: ' + this.left);
+  }
+  moveLeft() {
+    this.left -= this.speed;
+  }
+  moveUp() {
+    this.top -= this.speed;
+  }
+  moveDown() {
+    this.top += this.speed;
+  }
+}
+
+var hero = new Hero('ducbo.png', 20, 30, 200, 100);
+
+function start() {
+  if (hero.left < window.innerWidth - hero.size&& hero.top===20) {
+    hero.moveRight();
+  } else if (hero.top < window.innerHeight - hero.size && hero.left >= window.innerWidth - hero.size) {
+    hero.moveDown()
+  } else if (hero.left >30) {
+    hero.moveLeft();
+  } else if (hero.left>20) {
+    hero.moveUp();
+  }
+  document.getElementById('game').innerHTML = hero.getHeroElement();
+  setTimeout(start, 500)
+}
+
+start();
